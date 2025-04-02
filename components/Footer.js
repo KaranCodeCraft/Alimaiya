c0fa21c80e0e41289fabb23b646945618f3a46c4
dev <dev882624@gmail.com>
2025-04-02 16:40:49 +0530

    logo changed

--- components/Footer.js
+++ components/Footer.js
@@ -78,9 +78,9 @@ export default function Footer() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
-              <span className="ml-3 text-xl">Alimaiya</span>
+              <span className="ml-3 text-xl">Ally'Maiya</span>
             </Link>
-            <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Alimaiya
+            <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Ally'Maiya
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
               <Link href="#" className="text-gray-500">
